00c4fee121a54702b3973aeb20db459e8da92ddf
Khazi Nawaz Ahamed <khazina@jfrog.com>
2020-01-10 14:55:29 +0530

    added gitignore

--- node/.provision/node.sh
+++ node/.provision/node.sh
@@ -6,7 +6,7 @@ echo "source /home/vagrant/.nvm/nvm.sh" >> /home/vagrant/.profile
 source /home/vagrant/.profile
 
 nvm install node
-nvm install 8
-nvm alias default 8
+nvm install 13.6.0
+nvm alias default 13.6.0
 npm i -g ionic cordova
 
